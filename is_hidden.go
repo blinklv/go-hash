e73bd0c50bb1aaeeb08c7b79a31edb10e0245efa
blinklv <blinklv@icloud.com>
2019-12-04 14:38:48 +0800

    build: adjust build comments

--- is_hidden.go
+++ is_hidden.go
@@ -3,9 +3,10 @@
 // Author: blinklv <blinklv@icloud.com>
 // Create Time: 2019-10-31
 // Maintainer: blinklv <blinklv@icloud.com>
-// Last Change: 2019-10-31
+// Last Change: 2019-12-04
 
 // +build !windows
+
 package main
 
 // Check whether a file is hidden or not. (For Unix-Like System)
